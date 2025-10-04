d304707ab91ccebd30dd4f11de1bd8ded5dcebe7
Nhan-beo123 <nhanbeothcs@gmail.com>
2025-10-04 14:56:46 +0700

    aaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // Lưu ý dùng 'react-router-dom'
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
-import VehicleList from "./pages/Chon_xe";
+import VehicleList from "./pages/VehicleList";
+import TransportCompanies from "./pages/TransportCompanies";
+import PaymentQR from "./pages/PaymentQR";
+import PaymentHistory from "./pages/PaymentHistory";
+import CommodityForm from "./pages/CommodityForm";
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/verhicle_list" element={<VehicleList />} />
+          <Route path="/vehicle_list" element={<VehicleList />} />
+          <Route path="/commodity_form" element={<CommodityForm />} /> 
+          <Route path="/payment_history" element={<PaymentHistory />} />
+          <Route path="/paymentQR" element={<PaymentQR />} /> 
+          <Route path="/transport_companies" element={<TransportCompanies />} />  
         </Routes>
       </BrowserRouter>
     </>
