c948762dcd24b509df9f2d47528daeacf83b4e7b
PhongTan <tanhongphong30@gmail.com>
2025-10-05 15:10:02 +0700

    sua giao dien index

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      {" "}
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  const [taskBuffer, setTaskBuffer] = useState([]);
+  const [activeTaskCount, setActiveTaskCount] = useState(0);
+  const [completeTaskCount, setCompleteTaskCount] = useState(0);
+  const [filter, setFilter] = useState("all");
+  const [dateQuery, setDateQuery] = useState("today");
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchTasks();
+  }, [dateQuery]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [filter, dateQuery]);
+
+  // logic
+  const fetchTasks = async () => {
+    try {
+      const res = await api.get(`/tasks?filter=${dateQuery}`);
+      setTaskBuffer(res.data.tasks);
+      setActiveTaskCount(res.data.activeCount);
+      setCompleteTaskCount(res.data.completeCount);
+    } catch (error) {
+      console.error("Lỗi xảy ra khi truy xuất tasks:", error);
+      toast.error("Lỗi xảy ra khi truy xuất tasks.");
+    }
+  };
 };
 export default HomePage;
