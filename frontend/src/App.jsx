cfa87ff272b52660d5850786515d6e90634f185b
kokuhakufuyu <gnourt188@gmail.com>
2025-10-06 13:37:02 +0700

    ss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,30 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
-<<<<<<< HEAD
-import VehicleList from "./pages/Chon_xe";
-import Order from "./pages/Don_hang";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/vehicle_list" element={<VehicleList />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </>
-=======
 import VehicleList from "./pages/VehicleList";
 import TransportCompanies from "./pages/TransportCompanies";
 import PaymentQR from "./pages/PaymentQR";
 import PaymentHistory from "./pages/PaymentHistory";
-import CommodityForm from "./pages/CommodityForm"; 
-// ⬆️ Nếu dự án của bạn để CommodityForm trong "components",
-// đổi dòng trên thành:  import CommodityForm from "./components/CommodityForm";
+import CommodityForm from "./pages/CommodityForm";
+// Nếu CommodityForm để trong "components", đổi import trên thành:
+// import CommodityForm from "./components/CommodityForm";
 
 function App() {
   return (
@@ -54,7 +38,6 @@ function App() {
         <Route path="/commodityform" element={<CommodityForm />} />
       </Routes>
     </BrowserRouter>
->>>>>>> 84ef693f0d165d2ba8725b7014cb71363c4b6aa5
   );
 }
 
