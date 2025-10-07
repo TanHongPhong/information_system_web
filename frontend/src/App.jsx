b3255fd1246d18b9bbab7dc9b5aa064a7bb116b6
Nhan-beo123 <nhanbeothcs@gmail.com>
2025-10-07 11:20:29 +0700

    đã cập nhật route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import HomePage from "./pages/HomePage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import WarehouseInOut from "./pages/WarehouseInOut.jsx";
 import OrderTracking from "./pages/OrderTracking.jsx";
+import PaymentQR from "./pages/PaymentQR.jsx";
+import PaymentHistory from "./pages/PaymentHistory.jsx";
+import TransportCompanies from "./pages/TransportCompanies.jsx";
+import VehiclePages from "./pages/VehicleList.jsx";
 
 // Chỉ giữ 4 trang như bạn yêu cầu
 export default function App() {
@@ -14,6 +18,10 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/warehouse" element={<WarehouseInOut />} />
         <Route path="/order_tracking" element={<OrderTracking />} />
+        <Route path="/payment_history" element={<PaymentHistory />} />
+        <Route path="/payment_qr" element={<PaymentQR />} />
+        <Route path="/transport_companies" element={<TransportCompanies />} />
+        <Route path="/vehicle_list" element={<VehiclePages />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
