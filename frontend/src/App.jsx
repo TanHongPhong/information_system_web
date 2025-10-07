7da16f94ced1ad81025705e158ac229b67341986
lequangtruongK50 <truongle26007@gmail.com>
2025-10-07 19:44:40 +0700

    thông tin chỉnh sửa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,32 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import WarehouseInOut from "./pages/WarehouseInOut.jsx";
-import OrderTracking from "./pages/OrderTracking.jsx";
+// Pages (giữ cả 2 nhánh)
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
 
-// Chỉ giữ 4 trang như bạn yêu cầu
-export default function App() {
+import TransportCompanies from "./pages/TransportCompanies";
+import VehicleList from "./pages/VehicleList";
+import PaymentQR from "./pages/PaymentQR";
+import PaymentHistory from "./pages/PaymentHistory";
+import OrderTracking from "./pages/OrderTracking";
+import WareHouse  from "./pages/WareHouse";
+function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home & 404 */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/warehouse" element={<WarehouseInOut />} />
-        <Route path="/order_tracking" element={<OrderTracking />} />
         <Route path="*" element={<NotFound />} />
+        {/* Transport Companies */}
+        <Route path="/transport-companies" element={<TransportCompanies />} />
+        <Route path="/vehicle-list" element={<VehicleList />} />
+        <Route path="/payment-qr" element={<PaymentQR />} />
+        <Route path="/payment-history" element={<PaymentHistory />} />
+        <Route path="/order-tracking" element={<OrderTracking />} />
+        <Route path="/ware-house" element={<WareHouse />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
