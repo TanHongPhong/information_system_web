f0dee5ffb1f41522358c609f0afecf90e0a17dbe
PhongTan <tanhongphong30@gmail.com>
2025-10-08 20:44:45 +0700

    khong co gi het

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import WarehouseInOut from "./pages/WarehouseInOut.jsx";
@@ -8,21 +7,10 @@ import OrderTracking from "./pages/OrderTracking.jsx";
 import PaymentQR from "./pages/PaymentQR.jsx";
 import PaymentHistory from "./pages/PaymentHistory.jsx";
 import TransportCompanies from "./pages/TransportCompanies.jsx";
-import VehiclePages from "./pages/VehicleList.jsx";
-import OrderTrackingCustomner from "./pages/OrderTrackingCustomer.jsx"
+import VehicleList from "./pages/VehicleList.jsx";
+import OrderTrackingCustomner from "./pages/OrderTrackingCustomer.jsx";
 import Supplier from "./pages/Supplier.jsx";
 import OrderRequestDetails from "./pages/OrderRequestDetails.jsx";
-=======
-// Pages
-import HomePage from "./pages/HomePage";
-import NotFound from "./pages/NotFound";
-import WarehouseInOut from "./pages/WarehouseInOut";
-import OrderTracking from "./pages/OrderTracking";
-import TransportCompanies from "./pages/TransportCompanies";
-import PaymentHistory from "./pages/PaymentHistory";
-import PaymentQR from "./pages/PaymentQR";
-import VehicleList from "./pages/VehicleList";
->>>>>>> c5462653cbcb3d3e5708614818d9213e46703078
 
 function App() {
   return (
@@ -42,20 +30,17 @@ function App() {
         <Route path="/vehicle_list" element={<VehicleList />} />
         <Route path="/payment_history" element={<PaymentHistory />} />
         <Route path="/payment_qr" element={<PaymentQR />} />
-<<<<<<< HEAD
         <Route path="/transport_companies" element={<TransportCompanies />} />
-        <Route path="/vehicle_list" element={<VehiclePages />} />
-        <Route path="/order_tracking_customer" element={<OrderTrackingCustomner />} />
+        <Route
+          path="/order_tracking_customer"
+          element={<OrderTrackingCustomner />}
+        />
         <Route path="/supplier" element={<Supplier />} />
-=======
-        <Route path="/order_tracking" element={<OrderTracking />} />
-        <Route path="/warehouse_in_out" element={<WarehouseInOut />} />
-
-        {/* 404 cuối cùng */}
->>>>>>> c5462653cbcb3d3e5708614818d9213e46703078
         <Route path="*" element={<NotFound />} />
-        <Route path="/order_request_details" element={<OrderRequestDetails />} />
-  
+        <Route
+          path="/order_request_details"
+          element={<OrderRequestDetails />}
+        />
       </Routes>
     </BrowserRouter>
   );
